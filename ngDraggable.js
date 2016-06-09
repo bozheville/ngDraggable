d3905d8d195951052bf8c8695a6c16ff74b60773
Denys Grybov <d.grybov@plarium.com>
2016-06-09 12:27:52 +0300

    fix dimensions of clone element

--- ngDraggable.js
+++ ngDraggable.js
@@ -451,8 +451,8 @@ angular.module("ngDraggable", [])
                         scope.$apply(function () {
                             scope.clonedData = obj.data;
                         });
-                        element.css('width', obj.element[0].offsetWidth);
-                        element.css('height', obj.element[0].offsetHeight);
+                        element.css('width', obj.element[0].offsetWidth + 'px');
+                        element.css('height', obj.element[0].offsetHeight + 'px');
 
                         moveElement(obj.tx, obj.ty);
                     }
